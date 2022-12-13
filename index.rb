de9e1e43d63490b6f81457f58fbfd41adcc19644
Seamus Connolly <seamusconnolly@Seamuss-MacBook-Pro.local>
2022-12-13 21:59:26 +0700

    changign to php server

--- index.rb
+++ index.rb
@@ -1,15 +1,5 @@
-require 'sinatra'
-#require 'puma'
 require_relative 'telegram.rb'
-
 
 q = Telegram_.new
 q.start
 
-get '/' do
-    send_file 'trades.log'
-end
-
-get '/error' do
-    send_file 'logs.log'
-end
